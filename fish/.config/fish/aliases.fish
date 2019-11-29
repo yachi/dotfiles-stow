6bd31af234d8b389598bb798521a8b14a5b73c73
Yachi Lo <yaachi@gmail.com>
2019-11-29 14:02:12 +0800

    update kitty

--- fish/.config/fish/aliases.fish
+++ fish/.config/fish/aliases.fish
@@ -1,7 +1,29 @@
 alias v 'nvim -o'
+
 alias g git
 alias gst 'git status'
 alias gc 'git commit -v'
+alias gco 'git checkout'
 alias gca 'git commit -a'
 alias gd 'git diff'
 alias gdc 'git diff --cached'
+alias gp 'git push'
+
+alias bi 'bundler install -j256'
+alias bu 'bundler update'
+
+function gcob
+  git checkout (git for-each-ref --sort=committerdate refs/heads --format='%(refname:short)' | tail -r | fzf --height=30% --reverse --no-mouse)
+end
+
+function gcostmp
+  git branch -D stmona
+  git fetch
+  git checkout stmona
+  git merge --no-ff -
+  git push
+end
+
+function gmdd
+  git branch --merged | grep -v develop | grep -v master | xargs git branch -d
+end
--- fish/.config/fish/powerline.fish
+++ fish/.config/fish/powerline.fish
@@ -1,6 +1,6 @@
 if test (random 1 10) -gt 9
   echo 'updating powerline ...'
-  go get -u github.com/justjanne/powerline-go
+  go get -u github.com/justjanne/powerline-go &
 end
 
 function fish_prompt
