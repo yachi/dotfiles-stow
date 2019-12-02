6f9369d7884521e23d533d598160fd94b64650d4
Yachi Lo <yaachi@gmail.com>
2019-12-02 18:35:09 +0800

    add brew.sh

---
+++ brew.sh
@@ -0,0 +1,30 @@
+#/usr/bin/env zsh
+
+brew install \
+	asdf \
+	diff-so-fancy \
+	fzf \
+	git-flow-avh \
+	go \
+	gpg \
+	homebrew/cask-fonts/font-hack-nerd-font \
+	htop \
+	hub \
+	m-cli \
+	neovim \
+	parallel \
+	postgresql \
+	pwgen \
+	ripgrep \
+	ruby-install \
+	stow
+
+brew cask install \
+	docker \
+	font-hack-nerd-font \
+	istat-menus \
+	keybase \
+	kitty \
+	vagrant \
+	virtualbox \
+	virtualbox-extension-pack
